be0378f77836962e906b57edec8603fbb8588554
SangAhn Han <85681261+ddangahn2@users.noreply.github.com>
2024-09-22 12:06:33 +0900

    [level 1] Title: 폰켓몬, Time: 6.34 ms, Memory: 80.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int solution(int[] nums) {
+        return Arrays.stream(nums)
+                .boxed()
+                .collect(Collectors.collectingAndThen(Collectors.toSet(),
+                        phonekemons -> Integer.min(phonekemons.size(), nums.length / 2)));
+    }
+}

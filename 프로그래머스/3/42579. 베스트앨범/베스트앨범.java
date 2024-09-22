d6c5d770366d21e37e419edc27f514633ee235d3
SangAhn Han <85681261+ddangahn2@users.noreply.github.com>
2024-09-22 15:10:00 +0900

    [level 3] Title: 베스트앨범, Time: 14.62 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -3,44 +3,46 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
         int[] answer = {};
-        
         ArrayList<Integer> answerList = new ArrayList<>();
-
+        
         Map<String, Integer> genMap = new HashMap<>();
-        Map<String, ArrayList<ArrayList<Integer>>> songMap = new HashMap<>();
+        Map<String, ArrayList<ArrayList<Integer>>> playMap = new HashMap<>();
+
         for(int i=0; i<genres.length; i++) {
             genMap.computeIfAbsent(genres[i], k -> 0);
             genMap.put(genres[i], genMap.get(genres[i]) + plays[i]);
 
-            songMap.computeIfAbsent(genres[i], k -> new ArrayList<>());
-            ArrayList<Integer> song = new ArrayList<>();
-            song.add(i);
-            song.add(plays[i]);
-            songMap.get(genres[i]).add(song);
+            playMap.computeIfAbsent(genres[i], k -> new ArrayList<>());
+            ArrayList<Integer> play = new ArrayList<>();
+            play.add(i);
+            play.add(plays[i]);
+            playMap.get(genres[i]).add(play);
         }
 
-        List<Map.Entry<String, Integer>> genList = new ArrayList<>(genMap.entrySet());
-        genList.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+//        1. genMap sort
+        ArrayList<Map.Entry<String, Integer>> sortGenList = new ArrayList<>(genMap.entrySet());
+        sortGenList.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
 
-        for(Map.Entry<String, ArrayList<ArrayList<Integer>>> entry: songMap.entrySet()) {
-            ArrayList<ArrayList<Integer>> songs = entry.getValue();
+//        2. playMap sort
+        for(Map.Entry<String, ArrayList<ArrayList<Integer>>> map: playMap.entrySet()) {
+            ArrayList<ArrayList<Integer>> value = map.getValue();
 
-            songs.sort(Comparator
+            value.sort(Comparator
                     .comparing((ArrayList<Integer> arr) -> arr.get(1), Comparator.reverseOrder())
                     .thenComparing((ArrayList<Integer> arr) -> arr.get(0)));
         }
-
-        for(Map.Entry<String, Integer> key: genList) {
-            if (songMap.get(key.getKey()).size() == 1) {
-                answerList.add(songMap.get(key.getKey()).get(0).get(0));
+        
+//        3. 결과
+        for(Map.Entry<String, Integer> map: sortGenList) {
+            if (playMap.get(map.getKey()).size() == 1) {
+                answerList.add(playMap.get(map.getKey()).get(0).get(0));
             } else {
-                answerList.add(songMap.get(key.getKey()).get(0).get(0));
-                answerList.add(songMap.get(key.getKey()).get(1).get(0));
+                answerList.add(playMap.get(map.getKey()).get(0).get(0));
+                answerList.add(playMap.get(map.getKey()).get(1).get(0));
             }
         }
-
-        answer = answerList.stream().mapToInt(i -> i).toArray();
         
+        answer = answerList.stream().mapToInt(i -> i).toArray();
         return answer;
     }
 }

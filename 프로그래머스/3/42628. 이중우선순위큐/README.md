23363c8a63abebe2312f95cae873ec55593a7242
SangAhn Han <85681261+ddangahn2@users.noreply.github.com>
2024-09-23 15:21:40 +0900

    [level 3] Title: 이중우선순위큐, Time: 90.92 ms, Memory: 141 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/README.md
+++ 프로그래머스/3/42628. 이중우선순위큐/README.md
@@ -1,10 +1,10 @@
 # [level 3] 이중우선순위큐 - 42628 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42628) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42628#) 
 
 ### 성능 요약
 
-메모리: 117 MB, 시간: 72.12 ms
+메모리: 141 MB, 시간: 90.92 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 23일 15:14:32
+2024년 09월 23일 15:21:37
 
 ### 문제 설명
 
--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -4,88 +4,41 @@ class Solution {
     public int[] solution(String[] operations) {
         Queue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
         Queue<Integer> minHeap = new PriorityQueue<>();
-        Map<Integer, Integer> map = new HashMap<>();
         
-        StringTokenizer st;
         int total = 0;
         
         for(String operation: operations) {
-            st = new StringTokenizer(operation);
+            String[] st = operation.split(" ");
             
-            String opcode = st.nextToken();
-            int num = Integer.parseInt(st.nextToken());
+            int num = Integer.parseInt(st[1]);
             
-            if (opcode.equals("I")) {
-                map.computeIfAbsent(num, k -> 0);
-                map.put(num, map.get(num) + 1);
-                total += 1;
-                
+            if (st[0].equals("I")) {                
                 maxHeap.add(num);
                 minHeap.add(num);
             } 
             else {
-                if (total == 0) {
+                if(maxHeap.isEmpty()) {
                     continue;
                 }
-                total -= 1;
-                
                 if (num == 1) {
-                    while (true) {
-                        int maxNum = maxHeap.remove();
-                        int maxNumCount = map.get(maxNum);
-                        if (maxNumCount == 0) {
-                            continue;
-                        }
-                        else {
-                            map.put(maxNum, map.get(maxNum) - 1);
-                            break;
-                        }
-                    }
+                    int maxNum = maxHeap.remove();
+                    minHeap.remove(maxNum);
                 } 
                 else {
-                    while (true) {
-                        int minNum = minHeap.remove();
-                        int minNumCount = map.get(minNum);
-                        if (minNumCount == 0) {
-                            continue;
-                        }
-                        else {
-                            map.put(minNum, map.get(minNum) - 1);
-                            break;
-                        }
-                    }
+                    int minNum = minHeap.remove();
+                    maxHeap.remove(minNum);
                 }
             }
         }
         
         int[] answer = new int[2];
         
-        if (total == 0) {
+        if (maxHeap.isEmpty()) {
             answer[0] = 0;
             answer[1] = 0;
         } else {
-            while (true) {
-                int maxNum = maxHeap.remove();
-                int maxNumCount = map.get(maxNum);
-                if (maxNumCount == 0) {
-                    continue;
-                }
-                else {
-                    answer[0] = maxNum;
-                    break;
-                }
-            }
-            while (true) {
-                int minNum = minHeap.remove();
-                int minNumCount = map.get(minNum);
-                if (minNumCount == 0) {
-                    continue;
-                }
-                else {
-                    answer[1] = minNum;
-                    break;
-                }
-            }
+            answer[0] = maxHeap.remove();
+            answer[1] = minHeap.remove();
         }
         
         return answer;

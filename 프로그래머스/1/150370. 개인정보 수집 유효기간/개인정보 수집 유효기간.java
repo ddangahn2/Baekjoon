41a6a9bdc4b27d8f03f1a311db70b6acec5e5b23
SangAhn Han <85681261+ddangahn2@users.noreply.github.com>
2024-09-26 22:52:36 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 4.75 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,69 +1,35 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
-        ArrayList<Integer> arr = new ArrayList<>();
+        List<Integer> answer = new ArrayList<>();
         Map<String, Integer> termMap = new HashMap<>();
-        for(String term: terms) {
-            String[] termSplit = term.split(" ");
-            termMap.put(termSplit[0], Integer.parseInt(termSplit[1]));
+        int date = getDate(today);
+
+        for (String s : terms) {
+            String[] term = s.split(" ");
+
+            termMap.put(term[0], Integer.parseInt(term[1]));
         }
-        String[] todaySplit = today.split("\\.");
-        int tY = Integer.parseInt(todaySplit[0]);
-        int tM = Integer.parseInt(todaySplit[1]);
-        int tD = Integer.parseInt(todaySplit[2]);
-        
-        for(int i=1; i<=privacies.length; i++) {
-            String privacy = privacies[i-1];
-            String[] privacySplit = privacy.split(" ");
-            int getTerm = termMap.get(privacySplit[1]);
-            String[] privacyToday = privacySplit[0].split("\\.");
-            int PtY = Integer.parseInt(privacyToday[0]);
-            int PtM = Integer.parseInt(privacyToday[1]);
-            int PtD = Integer.parseInt(privacyToday[2]);
-            
-            boolean monthBeforeFlag = false;
-            if(PtD > 1) {
-                PtD -= 1;
-            }
-            else {
-                PtD = 28;
-                monthBeforeFlag = true;
-            }
-            
-            PtM += getTerm;
-            if(monthBeforeFlag) {
-                PtM -= 1;
-            }
-            if(PtM == 0) {
-                PtY -= 1;
-                PtM = 12;
-            }
-            while (PtM > 12) {
-                PtY += 1;
-                PtM -= 12;
-            }
-            
-            boolean flag = false;
-            if (tY > PtY) {
-                flag = true;
-            }else if(tY == PtY) {
-                if(tM > PtM) {
-                    flag = true;
-                } else if (tM == PtM) {
-                    if(tD > PtD) {
-                        flag = true;
-                    }
-                }
-            }
-            
-            if(flag) {
-                arr.add(i);
+        for (int i = 0; i < privacies.length; i++) {
+            String[] privacy = privacies[i].split(" ");
+
+            if (getDate(privacy[0]) + (termMap.get(privacy[1]) * 28) <= date) {
+                answer.add(i + 1);
             }
         }
-            
-        
-        int[] answer = arr.stream().mapToInt(i->i).toArray();
-        return answer;
+        return answer.stream().mapToInt(integer -> integer).toArray();
+    }
+
+    private int getDate(String today) {
+        String[] date = today.split("\\.");
+        int year = Integer.parseInt(date[0]);
+        int month = Integer.parseInt(date[1]);
+        int day = Integer.parseInt(date[2]);
+        return (year * 12 * 28) + (month * 28) + day;
     }
 }

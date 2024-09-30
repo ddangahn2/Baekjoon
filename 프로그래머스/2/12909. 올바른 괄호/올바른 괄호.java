9e02c913d71234ec39238b6df4f83474392569f0
SangAhn Han <85681261+ddangahn2@users.noreply.github.com>
2024-09-30 15:43:46 +0900

    [level 2] Title: 올바른 괄호, Time: 18.72 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -3,24 +3,23 @@ import java.util.*;
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
-
-        Stack<Character> stack = new Stack<>();
         
+        Stack<Character> stack = new Stack<>();
         for(int i=0; i<s.length(); i++) {
             if(s.charAt(i) == '(') {
                 stack.push('(');
-            } else {
-                if (stack.isEmpty()) {
+            }
+            else {
+                if(!stack.isEmpty()) {
+                    stack.pop();
+                }
+                else {
                     answer = false;
                     break;
                 }
-                if (stack.peek() == '(') {
-                    stack.pop();
-                }
             }
         }
-        
-        if (!stack.isEmpty()) {
+        if(!stack.isEmpty()){
             answer = false;
         }
         

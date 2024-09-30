ed5291df53e21100b87029b92bc75ca7ad33a7c6
SangAhn Han <85681261+ddangahn2@users.noreply.github.com>
2024-09-30 15:56:49 +0900

    [level 2] Title: 프로세스, Time: 1.77 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -3,31 +3,31 @@ import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
-        int queueLocation = 0;
-        Queue<int[]> queue = new LinkedList<>();
+        Queue<int[]> q = new LinkedList<>();
         Queue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
-
+        
         for(int i=0; i<priorities.length; i++) {
+            int[] queue = {i, priorities[i]};
+            q.add(queue);
             pq.add(priorities[i]);
-            queue.add(new int[]{i, priorities[i]});
         }
-        System.out.println(pq);
-        while(!pq.isEmpty()) {
-            int pri = pq.remove();
-            int index = 0;
-            while(true) {
-                int[] cur = queue.remove();
-                if (cur[1] == pri) {
-                    answer += 1;
-                    if (cur[0] == location) {
-                        return answer;
-                    }
-                    break;
-                } else {
-                    queue.add(cur);
+        
+        int pri = pq.remove();
+        
+        while(!q.isEmpty()) {
+            int[] queue = q.remove();
+            if(queue[1] == pri) {
+                answer += 1;
+                if(queue[0] == location) {
+                    return answer;
                 }
+                pri = pq.remove();
+            }
+            else {
+                q.add(queue);
             }
         }
+        
         return answer;
     }
 }

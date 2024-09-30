99f8f9cd33378e77a44d4968168d48c87f6d7005
SangAhn Han <85681261+ddangahn2@users.noreply.github.com>
2024-09-30 20:54:08 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 16.56 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -1,47 +1,32 @@
-import java.util.*;
+import java.util.HashMap;
 
 class Solution {
-    static Map<Character, Integer> map = new HashMap<>();
     public String solution(String[] survey, int[] choices) {
-        StringBuilder sb = new StringBuilder("");
-        
-        for(int i=0; i<survey.length; i++) {
-            char type1 = survey[i].charAt(0);
-            char type2 = survey[i].charAt(1);
-            
-            int score = choices[i] - 4;
-            if(score < 0) {
-                map.put(type1, map.getOrDefault(type1, 0) - score);
-            }
-            else {
-                map.put(type2, map.getOrDefault(type2, 0) + score);
+        String answer = "";
+        char [][] type = {{'R', 'T'}, {'C', 'F'}, {'J', 'M'}, {'A', 'N'}};
+        int [] score = {0, 3, 2, 1, 0, 1, 2, 3};
+        HashMap<Character, Integer> point = new HashMap<Character, Integer>();
+
+        // 점수 기록할 배열 초기화 
+        for (char[] t : type) {
+            point.put(t[0], 0);
+            point.put(t[1], 0);
+        }
+
+        // 점수 기록 
+        for (int idx = 0; idx < choices.length; idx++){
+            if(choices[idx] > 4){
+                point.put(survey[idx].charAt(1), point.get(survey[idx].charAt(1)) + score[choices[idx]]);
+            } else {
+                point.put(survey[idx].charAt(0), point.get(survey[idx].charAt(0)) + score[choices[idx]]);
             }
         }
-        typeChoose(sb, 'R', 'T');
-        typeChoose(sb, 'C', 'F');
-        typeChoose(sb, 'J', 'M');
-        typeChoose(sb, 'A', 'N');
-         
-        String answer = sb.toString();
+
+        // 지표 별 점수 비교 후 유형 기입
+        for (char[] t : type) {
+            answer += (point.get(t[1]) <= point.get(t[0])) ? t[0] : t[1];
+        }
+
         return answer;
-    }
-    public static void typeChoose(StringBuilder sb, char type1, char type2){
-        if(!map.containsKey(type1) && !map.containsKey(type2)) {
-            sb.append(type1);
-        }
-        else if(!map.containsKey(type1)) {
-            sb.append(type2);
-        }
-        else if(!map.containsKey(type2)) {
-            sb.append(type1);
-        }
-        else {
-            if(map.get(type1) < map.get(type2)) {
-                sb.append(type2);
-            }
-            else {
-                sb.append(type1);
-            }
-        }
     }
 }

301ce16411766a3e4654c5791e0fface7df30151
SangAhn Han <85681261+ddangahn2@users.noreply.github.com>
2024-10-01 15:37:31 +0900

    [level 2] Title: 모음 사전, Time: 12.87 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,19 +1,28 @@
 import java.util.*;
 
 class Solution {
-    static List<String> arr = new ArrayList<>();
+    static int count = 0;
+    static int result = 0;
     public int solution(String word) {
-        dfs("");
+        StringBuilder sb = new StringBuilder("");
+        dfs(sb, word);
         
-        return arr.indexOf(word);
+        return result;
     }
-    public void dfs(String s) {
-        if(s.length() > 5) {
+    public static void dfs(StringBuilder sb, String word) {
+        if(sb.length() > 5) {
             return;
         }
-        arr.add(s);
+        if(word.equals(sb.toString())) {
+            result = count;
+            return;
+        }
+        count += 1;
         for(int i=0; i<5; i++) {
-            dfs(s + "AEIOU".charAt(i));
+            int sbIdx = sb.length();
+            sb.append("AEIOU".charAt(i));
+            dfs(sb, word);
+            sb.deleteCharAt(sbIdx);
         }
     }
 }

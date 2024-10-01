faff1b713829369346aa257560b7fb99acb9c8f9
SangAhn Han <85681261+ddangahn2@users.noreply.github.com>
2024-10-01 15:27:19 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 1.47 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,37 +1,56 @@
 import java.util.*;
 
 class Solution {  
-    static Map<Integer, HashSet<Integer>> graph = new HashMap<>();
-    static int N;
-    static int result = 101;
+    static Map<Integer, ArrayList<Integer>> graph = new HashMap<>();
+    static int[] child;
     static boolean[] visited;
     public int solution(int n, int[][] wires) {
-        visited = new boolean[n];
-        N = n;
-        for(int[] wire: wires) {
-            graph.computeIfAbsent(wire[0], k -> new HashSet<>());
-            graph.computeIfAbsent(wire[1], k -> new HashSet<>());
+        int answer = 100;
+        int count = 100;
+        for(int[] wire: wires){
+            int node1 = wire[0];
+            int node2 = wire[1];
             
-            graph.get(wire[0]).add(wire[1]);
-            graph.get(wire[1]).add(wire[0]);
+            graph.computeIfAbsent(node1, k->new ArrayList<>());
+            graph.computeIfAbsent(node2, k->new ArrayList<>());
+            
+            graph.get(node1).add(node2);
+            graph.get(node2).add(node1);
         }
+        visited = new boolean[n+1];
+        child = new int[n+1];
         
         dfs(wires[0][0]);
         
-        return result;
-    }
-    static int dfs(int root) {
-        visited[root-1] = true;
+        // for(int i=0; i<child.length; i++){
+        //     System.out.println(i + ":" + child[i]);
+        // }
         
+        for(int[] wire: wires){
+            int node1 = wire[0];
+            int node2 = wire[1];
+            
+            if(child[node1] > child[node2]) {
+                count = Math.min(Math.abs(n - 2 * child[node2]), count);
+            }
+            else {
+                count = Math.min(Math.abs(n - 2 * child[node1]), count);
+            }
+            answer = Math.min(answer, count);
+        }
+        
+        return answer;
+    }
+    public static int dfs(int root) {
+        visited[root] = true;
         int count = 1;
         
-        for(Integer adjNode: graph.get(root)) {
-            if(!visited[adjNode - 1]) {
-                count += dfs(adjNode);
+        for(int node: graph.get(root)) {
+            if(!visited[node]) {
+                count += dfs(node);
             }
         }
-        result = Math.min(result, Math.abs(2*count-N));
-        
+        child[root] = count;
         return count;
     }
 }

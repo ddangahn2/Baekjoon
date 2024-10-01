8dfd48221730b6ed4f3a313e1025cbd9c8939bbe
SangAhn Han <85681261+ddangahn2@users.noreply.github.com>
2024-10-01 14:52:14 +0900

    [level 2] Title: 피로도, Time: 3.41 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,24 +1,24 @@
 class Solution {
-    static int ans;
     static boolean[] visited;
-
+    static int travel = 0;
+    static int answer = 0;
     public int solution(int k, int[][] dungeons) {
         visited = new boolean[dungeons.length];
+
+        dfs(k, dungeons, travel);
         
-        dfs(k, dungeons, 0);
-        
-        return ans;
+        return answer;
     }
-    public void dfs(int k, int[][] dungeons, int count) {
+    public static void dfs(int k, int[][] dungeons, int travel) {
+        if(travel > answer) {
+            answer = travel;
+        }
         for(int i=0; i<dungeons.length; i++) {
             if(!visited[i] && k >= dungeons[i][0]) {
                 visited[i] = true;
-                dfs(k - dungeons[i][1], dungeons, count + 1);
+                dfs(k - dungeons[i][1], dungeons, travel + 1);
                 visited[i] = false;
             }
-        }
-        if (ans < count) {
-            ans = count;
         }
     }
 }

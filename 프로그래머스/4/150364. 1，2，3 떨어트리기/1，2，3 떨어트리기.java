57db48777b551413a5b6553ae37e6935b7a35b66
SangAhn Han <85681261+ddangahn2@users.noreply.github.com>
2024-10-09 17:19:05 +0900

    [level 4] Title: 1,2,3 떨어트리기, Time: 7.34 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/4/150364. 1，2，3 떨어트리기/1，2，3 떨어트리기.java
+++ 프로그래머스/4/150364. 1，2，3 떨어트리기/1，2，3 떨어트리기.java
@@ -5,13 +5,10 @@ class Solution {
         Map<Integer, ArrayList<Integer>> graph = new HashMap<>();
         Map<Integer, Integer> nodePass = new HashMap<>();
         
-        Queue<Integer> nodeQ = new LinkedList<>();
-        List<Integer> leafNode = new ArrayList<>();
-        Map<Integer, Integer> leafNodeCnt = new HashMap<>();
-        
-        int match = 0;
-        
-        List<Integer> passWay = new ArrayList<>();
+//         leafNode 저장하는 arrayList
+//         leafNode에 들어간 target 개수 저장하는 거
+//         leafNode가 조건 만족했는지 확인하는거
+//         
         
         for(int[] edge: edges){
             int node1 = edge[0];
@@ -25,92 +22,14 @@ class Solution {
             nodePass.put(entry.getKey(), 0);
         }
         
-        nodeQ.add(1);
-        while(!nodeQ.isEmpty()){
-            int node = nodeQ.remove();
-            if(graph.containsKey(node)){
-                nodeQ.addAll(graph.get(node));
-            }
-            else{
-                leafNode.add(node);
-                leafNodeCnt.put(node, 0);
-            }
-        }
+//         leafNode에 하나씩 떨구기
+//         모든 leafNode 만족하는지 확인
+//         초과하면 그대로 끝
         
-        boolean[] leafNodeMatch = new boolean[leafNode.size()];
-        for(int i=0; i<leafNode.size(); i++){
-            int leaf = leafNode.get(i);
-            
-            if(target[leaf-1] == 0){
-                leafNodeMatch[leafNode.indexOf(leaf)] = true;
-                match += 1;
-            }
-        }
+//         숫자배정
+//         
         
-        boolean flag = true;
-        while(true){
-            int node = 1;
-            while(!leafNode.contains(node)){
-                int pass = nodePass.get(node);
-                nodePass.put(node, (pass+1) % graph.get(node).size());
-                node = graph.get(node).get(pass);
-            }
-            
-            int getCnt = leafNodeCnt.get(node);
-            if((getCnt + 1) <= target[node-1] && target[node-1] <= (getCnt + 1) * 3){
-                leafNodeCnt.put(node, leafNodeCnt.get(node) + 1);
-                passWay.add(node);
-        
-                if(!leafNodeMatch[leafNode.indexOf(node)]){
-                    leafNodeMatch[leafNode.indexOf(node)] = true;
-                    match += 1;
-                    
-                    if(match == leafNode.size()){
-                        break;
-                    }
-                }
-            }
-            else if ((getCnt + 1) > target[node-1]){
-                flag = false;
-                break;
-            }
-            else{
-                leafNodeCnt.put(node, leafNodeCnt.get(node) + 1);
-                passWay.add(node);
-            }
-        }
-        
-        int[] answer;
-        
-        if(flag){
-            answer = new int[passWay.size()];
-            for(int i=0; i<passWay.size(); i++){
-                int node = passWay.get(i);
-                
-                int nodeCnt = leafNodeCnt.get(node);
-                leafNodeCnt.put(node, leafNodeCnt.get(node) - 1);
-                int remain = target[node-1];
-                int num = 0;
-                
-                if(nodeCnt * 3 == remain){
-                    num = 3;
-                }
-                else if(nodeCnt * 3 - 1 == remain){
-                    num = 2;
-                }
-                else{
-                    num = 1;
-                }
-
-                target[node-1] -= num;
-                answer[i] = num;
-            }
-        }
-        else{
-            answer = new int[1];
-            answer[0] = -1;
-        }
-
+        int[] answer = {};
         return answer;
     }
 }

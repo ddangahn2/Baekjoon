6e0629ba2063195965f50f59c24f7c5ee42570ea
SangAhn Han <85681261+ddangahn2@users.noreply.github.com>
2024-10-12 19:59:51 +0900

    [level 3] Title: 표 병합, Time: 20.55 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/3/150366. 표 병합/표 병합.java
+++ 프로그래머스/3/150366. 표 병합/표 병합.java
@@ -5,106 +5,113 @@ class Solution {
     static Map<Integer, String> map = new HashMap<>();
     
     public String[] solution(String[] commands) {
-        ArrayList<String> arr = new ArrayList<>();
-        for(int i=0; i<2500; i++){
-            p[i] = i;
-        }
+        for(int i=0; i<p.length; i++) p[i] = i;
+
+        List<String> answerList = new ArrayList<>();
         
-        for(int t=0; t<commands.length; t++){
-            String[] command = commands[t].split(" ");    
-            
-            if(command[0].equals("UPDATE")){
-                if(command.length == 4){
-                    int r = Integer.parseInt(command[1]) - 1;
-                    int c = Integer.parseInt(command[2]) - 1;
-                    String value = command[3];
+        for(String command: commands){
+            String[] line = command.split(" ");
+     
+            if("UPDATE".equals(line[0])){
+                if(line.length == 4){
+                    int r = Integer.parseInt(line[1]) - 1;
+                    int c = Integer.parseInt(line[2]) - 1;
+                    String value = line[3];
                     
-                    int parent = findParent(50*r + c);
-                    map.put(parent, value);
+                    int idx = r*50 + c;
+                    
+                    int findIdx = find(idx);
+                    map.put(findIdx, value);
                 }
                 else{
-                    String value1 = command[1];
-                    String value2 = command[2];
+                    String value1 = line[1];
+                    String value2 = line[2];
                     
-                    for(Map.Entry<Integer, String> mapEntry: map.entrySet()){
-                        int key = mapEntry.getKey();
-                        String value = mapEntry.getValue();
-                        
-                        if(value.equals(value1)) {
-                            map.put(key, value2);
-                        }
-                    }
+                    changeValue(value1, value2);
                 }
             }
-            else if(command[0].equals("MERGE")){
-                int r1 = Integer.parseInt(command[1]) - 1;
-                int c1 = Integer.parseInt(command[2]) - 1;
-                int r2 = Integer.parseInt(command[3]) - 1;
-                int c2 = Integer.parseInt(command[4]) - 1;
+            else if("MERGE".equals(line[0])){
+                int r1 = Integer.parseInt(line[1]) - 1;
+                int c1 = Integer.parseInt(line[2]) - 1;
+                int r2 = Integer.parseInt(line[3]) - 1;
+                int c2 = Integer.parseInt(line[4]) - 1;
                 
-                int parent1 = findParent(r1*50 + c1);
-                int parent2 = findParent(r2*50 + c2);
+                int idx1 = r1*50 + c1;
+                int idx2 = r2*50 + c2;
                 
-                if(parent1 == parent2) {
-                    continue;
-                }
-                if (map.containsKey(parent1)) {
-                    p[parent2] = parent1;
-                    if(map.containsKey(parent2)){
-                        map.remove(parent2);
-                    }
-                } else {
-                    p[parent1] = parent2;
-                    if(map.containsKey(parent1)){
-                        map.remove(parent1);
-                    }
-                }
+                int findIdx1 = find(idx1);
+                int findIdx2 = find(idx2);
+                
+                merge(findIdx1, findIdx2);
             }
-            else if(command[0].equals("UNMERGE")){
-                int r = Integer.parseInt(command[1]) - 1;
-                int c = Integer.parseInt(command[2]) - 1;
+            else if("UNMERGE".equals(line[0])){
+                int r = Integer.parseInt(line[1]) - 1;
+                int c = Integer.parseInt(line[2]) - 1;
                 
-                int parent1 = findParent(r*50 + c);
+                int idx = r*50 + c;
                 
-                if(map.containsKey(parent1)) {
-                    String value = map.get(parent1);
-                    map.remove(parent1);
-                    map.put(r*50 + c, value);
-                }
-                ArrayList<Integer> unmerges = new ArrayList<>();
-                
-                for(int i=0; i<50; i++) {
-                    for(int j=0; j<50; j++) {
-                        if(findParent(i*50+j) == parent1) {
-                            unmerges.add(i*50 + j);
-                        }
-                    }
-                }
-                for(int unmerge: unmerges) {
-                    p[unmerge] = unmerge;
-                }
+                int findIdx = find(idx);
+                unmerge(findIdx, idx);
             }
-            else{
-                int r = Integer.parseInt(command[1]) - 1;
-                int c = Integer.parseInt(command[2]) - 1;
+            else if("PRINT".equals(line[0])){
+                int r = Integer.parseInt(line[1]) - 1;
+                int c = Integer.parseInt(line[2]) - 1;
                 
-                int parent = findParent(r*50 + c);
-                if(map.containsKey(parent)) {
-                    arr.add(map.get(parent));
-                } else {
-                    arr.add("EMPTY");
-                }
+                int idx = r*50 + c;
+                int findIdx = find(idx);
+                
+                if(map.containsKey(findIdx)) answerList.add(map.get(findIdx));
+                else answerList.add("EMPTY");
             }
         }
-       
-        String[] answer = arr.toArray(new String[0]);
+ 
+        String[] answer = new String[answerList.size()];
+        answer = answerList.toArray(answer);
         return answer;
     }
-    public static int findParent(int child) {
-        int parent = p[child];
-        while(parent != p[parent]) {
-            parent = p[parent];
+    public static int find(int idx){
+        if(idx != p[idx]){
+            p[idx] = find(p[idx]);
         }
-        return parent;
+        
+        return p[idx];
+    }
+    public static void changeValue(String v1, String v2){
+        for(Map.Entry<Integer, String> entry: map.entrySet()){
+            if(entry.getValue().equals(v1)){
+                map.put(entry.getKey(), v2);
+            }
+        }
+    }
+    public static void merge(int idx1, int idx2){
+        if(idx1 == idx2) return;
+        
+        if(map.containsKey(idx1)){
+            p[idx2] = idx1;
+            if(map.containsKey(idx2)){
+                map.remove(idx2);
+            }
+        }
+        else if(map.containsKey(idx2)){
+            p[idx1] = idx2;
+        }
+        else{
+            p[idx2] = idx1;
+        }
+
+    }
+    public static void unmerge(int parent, int idx){
+        List<Integer> parentIdx = new ArrayList<>();
+        
+        for(int i=0; i<p.length; i++){
+            if(find(i) == parent) parentIdx.add(i);
+        }
+        for(int i: parentIdx){
+            p[i] = i;
+        }
+        if(map.containsKey(parent)){
+            String value = map.remove(parent);
+            map.put(idx, value);
+        }
     }
 }

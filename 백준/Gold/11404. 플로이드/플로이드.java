0721bf82ab437d4dfd7f63f827e5af0dae7aa392
SangAhn Han <85681261+ddangahn2@users.noreply.github.com>
2025-02-13 15:21:20 +0900

    [Gold IV] Title: 플로이드, Time: 308 ms, Memory: 41564 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,42 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int INF = 1000000000;
-    static int[][] board;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+    public static StringTokenizer st;
+    public static StringBuilder sb = new StringBuilder();
 
-        board = new int[n][n];
-        for(int i=0; i<n; i++){
-            Arrays.fill(board[i], INF);
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        int[][] board = new int[N][N];
+        for(int i=0; i<N; i++) {
+            Arrays.fill(board[i], Integer.MAX_VALUE);
             board[i][i] = 0;
         }
 
-        for(int i=0; i<m; i++){
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            int c = sc.nextInt();
+        for(int i=0; i<M; i++) {
+            st = new StringTokenizer(br.readLine());
 
-            board[a-1][b-1] = Math.min(board[a-1][b-1], c);
+            int a = Integer.parseInt(st.nextToken())-1;
+            int b = Integer.parseInt(st.nextToken())-1;
+            int c = Integer.parseInt(st.nextToken());
+
+            board[a][b] = Math.min(board[a][b], c);
         }
 
-        for(int k=0; k<n; k++){
-            for(int i=0; i<n; i++){
-                for(int j=0; j<n; j++){
+        for(int k=0; k<N; k++) {
+            for(int i=0; i<N; i++) {
+                for(int j=0; j<N; j++) {
+                    if(board[i][k] == Integer.MAX_VALUE || board[k][j] == Integer.MAX_VALUE) continue;
+
                     board[i][j] = Math.min(board[i][j], board[i][k] + board[k][j]);
                 }
             }
         }
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(board[i][j] == INF) board[i][j] = 0;
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
 
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<N; j++) {
+                if(board[i][j] == Integer.MAX_VALUE) sb.append(0);
+                else sb.append(board[i][j]);
+                sb.append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
 }

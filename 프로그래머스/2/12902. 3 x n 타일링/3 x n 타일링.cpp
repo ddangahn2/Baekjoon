f7710799cb45c91b2c6dee5af45539122c065e9f
SangAhn Han <85681261+ddangahn2@users.noreply.github.com>
2025-04-25 18:50:15 +0900

    [level 2] Title: 3 x n 타일링, Time: 0.07 ms, Memory: 3.85 MB -BaekjoonHub

--- 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.cpp
+++ 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.cpp
@@ -5,28 +5,5 @@ using namespace std;
 
 int solution(int n) {
     int answer = 0;
-    vector<int> v1;
-    vector<int> v2;
-    
-    v1.push_back(3);
-    v1.push_back(11);
-    v2.push_back(1);
-    v2.push_back(4);
-    
-    for(int i=2; i<=n/2; i++) {
-        int temp_v1 = 0;
-        int temp_v2 = 0;
-        temp_v2 += v1[i-1];
-        temp_v2 += v2[i-1];
-        temp_v2 %= 1000000007;
-        v2.push_back(temp_v2);
-        
-        temp_v1 = 2 * temp_v2;
-        temp_v1 %= 1000000007;
-        temp_v1 += v1[i-1];
-        temp_v1 %= 1000000007;
-        v1.push_back(temp_v1);
-    }
-    
-    return v1[n/2 - 1];
+    return answer;
 }
